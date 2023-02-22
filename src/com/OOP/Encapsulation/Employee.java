d35e4edf941fe1537bfbfd225539d992b7dc2e32
Hanzala Ahmed <hanzhanzala@gmail.com>
2023-02-22 13:58:39 +0530

    OOP Java Encapsulating Class

---
+++ src/com/OOP/Encapsulation/Employee.java
@@ -0,0 +1,9 @@
+package com.OOP.Encapsulation;
+
+public class Employee {
+    public int baseSalary;
+    public int hourlyRate;
+    public int calculateWage(int extraHours){
+        return baseSalary+(hourlyRate*extraHours);
+    }
+}
---
+++ src/com/OOP/Encapsulation/Main.java
@@ -0,0 +1,11 @@
+package com.OOP.Encapsulation;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+        employee.baseSalary = 50_000;
+        employee.hourlyRate = 20;
+        int wage = employee.calculateWage(10);
+        System.out.println(wage);
+    }
+}
