933924e5047d5271506c0ed7a2e18df489825dbc
Hanzala Ahmed <hanzhanzala@gmail.com>
2023-02-22 15:05:09 +0530

    OOP Java Encapsulating Class

--- src/com/OOP/Encapsulation/Employee.java
+++ src/com/OOP/Encapsulation/Employee.java
@@ -1,9 +1,27 @@
 package com.OOP.Encapsulation;
 
 public class Employee {
-    public int baseSalary;
-    public int hourlyRate;
+    private int baseSalary;
+    private int hourlyRate;
     public int calculateWage(int extraHours){
-        return baseSalary+(hourlyRate*extraHours);
+        return getBaseSalary()+(getHourlyRate() *extraHours);
+    }
+    public void setBaseSalary(int baseSalary){
+        if(baseSalary<=0)
+            throw new IllegalArgumentException("Salary cannot be zero or less.");
+        this.baseSalary=baseSalary;
+    }
+    public int getBaseSalary(){
+        return baseSalary;
+    }
+
+    public int getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        if(hourlyRate<=0)
+            throw new IllegalArgumentException("Hourly Rate cannot be Zero");
+        this.hourlyRate = hourlyRate;
     }
 }
