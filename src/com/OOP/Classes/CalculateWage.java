3d6297c8ca87c36b8dd6dae8993642c8d20ba196
Hanzala Ahmed <hanzhanzala@gmail.com>
2023-02-22 13:41:40 +0530

    OOP Java Classes

---
+++ src/com/OOP/Classes/CalculateWage.java
@@ -0,0 +1,18 @@
+package com.OOP.Classes;
+
+public class CalculateWage {
+    public static void main(String[] args) {
+        int baseSalary=50_000;
+        int extraHours=10;
+        int hourlyRate=20;
+        int wage=calculateWage(baseSalary,extraHours,hourlyRate);
+        System.out.println(wage);
+    }
+    public static int calculateWage(
+            int baseSalary,
+            int extraHours,
+            int hourlyRate
+    ){
+        return baseSalary+(extraHours*hourlyRate);
+    }
+}
--- src/com/OOP/Classes/Main.java
+++ src/com/OOP/Classes/Main.java
@@ -1,6 +1,20 @@
 package com.OOP.Classes;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        TextBox textBox1=new TextBox();
+        textBox1.setText("Box 1");
+        System.out.println(textBox1.text.toUpperCase());
+
+        textBox1.clear();
+        System.out.println(textBox1.text+"Gayab");
+
+        TextBox textBox2=new TextBox();
+        textBox2.setText("Box 2");
+        System.out.println(textBox2.text);
+
+        TextBox textBox3=textBox1;
+        textBox3.setText("Box 1 and Box 3 are pointing to the same object...");
+        System.out.println(textBox1.text);
     }
 }
